9193bc258323a75f5d1c9b073b6377608deb4833
joao505 <160245421+joao505@users.noreply.github.com>
2024-05-03 09:46:55 -0300

    Add files via upload

---
+++ cac/exer1.js
@@ -0,0 +1,36 @@
+//1) Mostre um alerta com a mensagem "Bem-vindo ao nosso site!".
+window.alert("Bem-vindo ao nosso site!")
+
+//2) Declare uma variável chamada 'nome' e atribua a ela o valor "Lua".
+var nome = "lua"
+
+//3) Crie uma variável chamada 'idade' e atribua a ela o valor 25.
+var idade = '25'
+
+//4) Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+var idade = '50'
+
+//5) Defina uma variável 'saldoDisponivel' e atribua a ela o valor 1000.
+var saldoDisponivel = 1000
+
+//6) Exiba um alerta com o texto "Erro! Preencha todos os campos."
+window.alert("Erro! Preencha todos os campos.")
+
+/*7) Declare uma variável chamada mensagemDeErro e atribua a ela o valor 
+"Erro! Preencha todos os campos." Agora exiba um alerta com o valor da variável
+mensagemDeErro.*/
+var mensagemDeErro = "Erro! Preencha todos os campos."
+window.alert(mensagemDeErro);
+
+//8)Use um prompt para perguntar o nome do usuário e armazená-lo na variável "nome".
+var nome = window.prompt("Qual seu nome?")
+
+//9) Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+var idade = window.prompt("Qual sua idade?")
+
+/*10) Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade. 
+Agora, a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".*/
+var idade = window.prompt("Qual sua idade?")
+    if (idade => 18) {
+        window.alert("Pode tirar habilitação!")
+    }
---
+++ cac/exer2.js
@@ -0,0 +1,13 @@
+//1) Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+var n1 = 0
+while (n1 <= 10){
+    console.log(n1)
+    n1 += 1
+}
+
+// 2) Crie um contador que comece em 10 e vá até 0 usando um loop while. Mostre cada número.
+var n1 = 10
+while (n1 = 0){
+    console.log(n1)
+    n1 -= 1
+}
