cd1e24276eca5712c83b3bad7c9c09a9d1262756
joao505 <160245421+joao505@users.noreply.github.com>
2024-06-20 18:26:55 -0500

    Add files via upload

--- aula/tabela_periodica .py
+++ aula/tabela_periodica .py
@@ -1,95 +1,254 @@
+#proje tabela:
+
+#1parte separar os elementos:
+
+#1
 h = {
     "simbulo":"H",
     "nome": "Hidrogênio",
     "numero atomico": "01",
-    "massa atomica": "1.008",
-    "conf eletronica": "1s1"
+    "massa atomica": "1.0",
+    "tipo": "hidrogenio"
 }
 
+#2
 he = {
     "simbulo": "He",
     "nome": "hélio",
     "numero atomico": "02",
-    "massa atomica": "4.003",
-    "conf eletronica": '1s1'
+    "massa atomica": "4.0",
+    "tipo": "gás nobre"
 }
 
+#3
 li = {
     "simbulo": "Li",
     "nome": "lítio",
     "numero atomico": "03",
-    "massa atomica": "6.941",
-    "conf eletronica": "1s1, 2s1"
+    "massa atomica": "6.9",
+    "tipo": "metal"
 }
 
+#4
 be = {
     "simbulo": "Be",
     "nome": "Berílio",
     "numero atomico": "04",
-    "massa atomica": "9.012",
-    "conf eletronica": '1s2, 2s2'
+    "massa atomica": "9.0",
+    "tipo": "metal"
 }
- 
+
+#5
 b = {
     "simbulo": "B",
     "nome": "Boro",
     "numero atomico": "05",
-    "massa atomica": "10.811",
-    "conf eletronica": '1s2, 2s2, 2p1'
+    "massa atomica": "10.8",
+    "tipo": "semi-metal"
 }
 
+#6
 c = {
     "simbulo": "C",
     "nome": "Carbono",
     "numero atomico": "06",
-    "massa atomica": "12.011",
-    "conf eletronica": '1s2, 2s2, 2p2'
+    "massa atomica": "12.0",
+    "tipo": "não-metal"
 }
 
+#7
 n = {
     "simbulo": "N",
     "nome": "Nitrogenio",
     "numero atomico": "07",
-    "massa atomica": "14.007",
-    "conf eletronica": '1s2,2s2,2p3'
+    "massa atomica": "14.0",
+    "tipo": "não-metal"
 }
 
+#8
 o = {
     "simbulo": "O",
     "nome": "Oxigenio",
     "numero atomico": "08",
-    "massa atomica": "15.999",
-    "conf eletronica": '1s2,2s2,2p4'
+    "massa atomica": "15.9",
+    "tipo": "não-metal"
 }
 
+#9
 f = {
     "simbulo": "F",
     "nome": "Flúor",
     "numero atomico": "09",
-    "massa atomica": "18.998",
-    "conf eletronica": '1s2,2s2,2p5'
+    "massa atomica": "18.9",
+    "tipo": "não-metal"
 }
-
+#10
 ne = {
     "simbulo": "Ne",
     "nome": "Neonio",
     "numero atomico": "10",
-    "massa atomica": "20.18",
-    "conf eletronica": '1s2,2s2,2p6'
+    "massa atomica": "20.1",
+    "tipo": "gás nobre"
 }
 
+#11
 na = {
     "simbulo": "Na",
     "nome": "Sódio",
     "numero atomico": "11",
-    "massa atomica": "22.99",
-    "conf eletronica": '[ne]3s1'
+    "massa atomica": "22.9",
+    "tipo": "metal"
 }
 
+#12
 mg ={
     "simbulo": "Mg",
-    "nome": "hélio",
-    "numero atomico": "02",
-    "massa atomica": "4.003",
-    "conf eletronica": '1s1'
+    "nome": "Magnésio",
+    "numero atomico": "12",
+    "massa atomica": "24.3",
+    "tipo": "metal"
+}
+
+#13
+al ={
+    "simbulo": "Al",
+    "nome": "Alumínio",
+    "numero atomico": "13",
+    "massa atomica": "27.0",
+    "tipo": "metal"
+}
+
+#14
+si ={
+    "simbulo": "Si",
+    "nome": "Silício",
+    "numero atomico": "14",
+    "massa atomica": "28.1",
+    "tipo": "semi-metal"
+}
+
+#15
+p ={
+    "simbulo": "P",
+    "nome": "Fósforo",
+    "numero atomico": "15",
+    "massa atomica": "31.0",
+    "tipo": "não-metal"
+}
+
+#16
+s ={
+    "simbulo": "S",
+    "nome": "enxofre",
+    "numero atomico": "16",
+    "massa atomica": "32.1",
+    "tipo": "não-metal"
+}
+
+#17
+cl ={
+    "simbulo": "Cl",
+    "nome": "Cloro",
+    "numero atomico": "17",
+    "massa atomica": "35.5",
+    "tipo": "não-metal"
+}
+
+#18
+ar ={
+    "simbulo": "Ar",
+    "nome": "Argonio",
+    "numero atomico": "18",
+    "massa atomica": "39.9",
+    "tipo": "gás nobre"
+}
+
+#19
+k ={
+    "simbulo": "K",
+    "nome": "Potássio",
+    "numero atomico": "19",
+    "massa atomica": "39.1",
+    "tipo": "metal"
+}
+
+#20
+ca ={
+    "simbulo": "Ca",
+    "nome": "cálcio",
+    "numero atomico": "20",
+    "massa atomica": "40.1",
+    "tipo": "metal"
+}
+
+#21
+sc ={
+    "simbulo": "Sc",
+    "nome": "Escandio",
+    "numero atomico": "21",
+    "massa atomica": "45.0",
+    "tipo": "metal"
+}
+
+#22
+ti ={
+    "simbulo": "Ti",
+    "nome": "Titaneo",
+    "numero atomico": "22",
+    "massa atomica": "47.9",
+    "tipo": "metal"
+}
+
+#23
+v ={
+    "simbulo": "V",
+    "nome": "Vanádio",
+    "numero atomico": "23",
+    "massa atomica": "50.9",
+    "tipo": "metal"
+}
+
+#24
+cr ={
+    "simbulo": "Cr",
+    "nome": "Cromio",
+    "numero atomico": "24",
+    "massa atomica": "52.0",
+    "tipo": "metal"
+}
+
+#25
+mn ={
+    "simbulo": "Mn",
+    "nome": "Manganes",
+    "numero atomico": "25",
+    "massa atomica": "54.9",
+    "tipo": "metal"
+}
+
+#26
+fe ={
+    "simbulo": "Fe",
+    "nome": "Ferro",
+    "numero atomico": "26",
+    "massa atomica": "55.8",
+    "tipo": "metal"
+}
+
+#27
+co ={
+    "simbulo": "Co",
+    "nome": "Cobalto",
+    "numero atomico": "27",
+    "massa atomica": "58.9",
+    "tipo": "metal"
+}
+
+#28
+ni ={
+    "simbulo": "K",
+    "nome": "Potássio",
+    "numero atomico": "19",
+    "massa atomica": "39.1",
+    "tipo": "metal"
 }
